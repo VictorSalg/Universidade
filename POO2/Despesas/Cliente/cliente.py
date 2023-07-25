de87748022b7d90fb3cec587c5baee646d6b166f
VictorSalg <140073180+VictorSalg@users.noreply.github.com>
2023-07-25 09:15:50 -0300

    Delete POO2/Despesas directory

--- POO2/Despesas/Cliente/cliente.py
+++
@@ -1,9 +0,0 @@
-import socket 
-
-def cliente():
-    ip = 'localhost'
-    port = 5001
-    addr = ((ip, port)) 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    client_socket.connect(addr)
-    return client_socket
